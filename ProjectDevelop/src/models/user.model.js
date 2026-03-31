9be4136275957ea57cec47cf0bd0680dee6e311c
Suman Pun Magar <mannmagar19@gmail.com>
2026-03-31 21:19:43 +0545

    Created the model for video

--- ProjectDevelop/src/models/user.model.js
+++ ProjectDevelop/src/models/user.model.js
@@ -1,57 +1,51 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim:true,
-        index: true,
-
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim:true,
-
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    fullname:{
-        type: String,
-        required: true,
-        trim:true,
-        index: true,
-
-    },    
-    avatar:{
-        type: String, // cloudinary url
-        required: true,
-
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
     },
 
-    coverImage:{
-        type: String, // cloudinary url
-
+    coverImage: {
+      type: String, // cloudinary url
     },
-    watchHistory:[{
+    watchHistory: [
+      {
         type: Schema.Types.ObjectId,
-        ref:"Video"
-
-    }],
-    password:{
-        type: String, 
-        required: [true, 'Password is required']
-
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
     },
-    refreshToken:{
-        type: String
-
+    refreshToken: {
+      type: String,
     },
-    
-},{timestamps:true})
+  },
+  { timestamps: true }
+);
 
-
-
-export const User = mongoose.model("User", userSchema)
+export const User = mongoose.model("User", userSchema);
